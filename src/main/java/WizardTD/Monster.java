1956bd7a73e12ff5a9cbf51dd70a13fb06de89ab
Ian Chang <ycha5975@uni.sydney.edu.au>
2023-09-26 16:41:16 +1000

    monster moves at the right speed

--- src/main/java/WizardTD/Monster.java
+++ src/main/java/WizardTD/Monster.java
@@ -5,12 +5,14 @@ import processing.core.PImage;
 import java.util.List;
 import java.util.ArrayList;
 import WizardTD.subtiles.*;
+import WizardTD.PathFinder.Node;
 
 
 public class Monster {
 
     private PImage image;
-    private int x, y;  // Position of the monster
+    
+    private float x, y, targetX, targetY; 
     private Board board;
     private String direction = "";  // Can be "up", "down", "left", "right"
     List<String> pathToWizard;
@@ -24,8 +26,8 @@ public class Monster {
         this.image = app.loadImage("src/main/resources/WizardTD/gremlin.png");
         List<int[]> spawnPoints = getSpawnPoints();
         int[] chosenSpawn = spawnPoints.get((int) (Math.random() * spawnPoints.size()));
-        this.x = chosenSpawn[0];
-        this.y = chosenSpawn[1];
+        this.x = (float) chosenSpawn[0];
+        this.y = (float) chosenSpawn[1];
         this.speed = speed;
     }
 
@@ -38,7 +40,7 @@ public class Monster {
         float totalMove = speed + leftoverMove;
         int intMove = (int) totalMove;  // integer part of the movement
         leftoverMove = totalMove - intMove;  // store the fractional part for next frame
-    
+        
         for (int i = 0; i < intMove; i++) {
             move();  // call the existing move method
         }
@@ -80,7 +82,7 @@ public class Monster {
     
     public void move() {
         Tile[][] tiles = board.getTiles();
-    
+        
         if (x < 0 || x >= tiles[0].length || y < 0 || y >= tiles.length) {
             moveOutside(tiles);
         } else {
@@ -100,16 +102,16 @@ public class Monster {
     private void moveInDirection() {
         switch (direction) {
             case "up":
-                y--;
+                y -= 1.0f / 32;
                 break;
             case "down":
-                y++;
+                y += 1.0f / 32;
                 break;
             case "left":
-                x--;
+                x -= 1.0f / 32;
                 break;
             case "right":
-                x++;
+                x += 1.0f / 32;
                 break;
         }
     }
@@ -121,42 +123,55 @@ public class Monster {
      *
      * For details on the A* algorithm, refer to the credits in the PathFinder class.
      */
-    private void moveInside(Tile[][] tiles) {
-        PathFinder.Node start = pathFinder.new Node(x, y);
-        
-        // Find the goal (the 'W' tile)
-        int goalX = -1, goalY = -1;
-        for (int i = 0; i < tiles.length; i++) {
-            for (int j = 0; j < tiles[i].length; j++) {
-                if (tiles[i][j] instanceof WizardHouseTile) {
-                    goalX = j;
-                    goalY = i;
-                    break;
-                }
-            }
-            if (goalX != -1) {
-                break;
+     private void moveInside(Tile[][] tiles) {
+         // If the monster is close to its target or doesn't have a target, determine the next tile
+         if (Math.abs(x - targetX) < 0.01 && Math.abs(y - targetY) < 0.01 || targetX == 0 && targetY == 0) {
+             Node start = pathFinder.new Node((int)x, (int)y);  // Create a start node based on the monster's current position
+             Node goal = null;
+             
+             // Loop through the tiles to find the WizardHouseTile and set the goal node to its position
+             for (int i = 0; i < tiles.length; i++) {
+                 for (int j = 0; j < tiles[0].length; j++) {
+                     if (tiles[i][j] instanceof WizardHouseTile) {
+                         goal = pathFinder.new Node(j, i);  // Update the goal node based on the WizardHouseTile's position
+                     }
+                 }
+             }
+    
+            // Use the PathFinder's findPath method to get the path to the wizard house
+            List<Node> path = pathFinder.findPath(start, goal, tiles);
+    
+            // If the path is valid and has more than one node, set the next tile in the path as the monster's target
+            if (path != null && path.size() > 1) {
+                Node nextTile = path.get(1);
+                targetX = nextTile.x;
+                targetY = nextTile.y;
+            } else {
+                // If the monster is already at the goal or there's no path, set the target as the current position
+                targetX = x;
+                targetY = y;
             }
         }
-        
-        PathFinder.Node goal = pathFinder.new Node(goalX, goalY);
-        
-        List<PathFinder.Node> path = pathFinder.findPath(start, goal, tiles);
-        
-        if (path.size() > 1) {
-            PathFinder.Node nextStep = path.get(1);
-            this.x = nextStep.x;
-            this.y = nextStep.y;
+    
+        // Move towards the target in pixel increments, as before
+        float deltaX = targetX - x;
+        float deltaY = targetY - y;
+        float distance = (float) Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        if (distance > 0) {
+            float moveX = (speed / 32.0f) * (deltaX / distance);
+            float moveY = (speed / 32.0f) * (deltaY / distance);
+            x += moveX;
+            y += moveY;
         }
-    }       
+    }
+    
     
 
     public void render(PApplet app) {
-        if (x != -1 && y != -1) { // Only render if monster hasn't disappeared
-            int drawX = x * App.CELLSIZE + App.CELLSIZE / 2 - image.width / 2;
-            int drawY = y * App.CELLSIZE + App.CELLSIZE / 2 - image.height / 2 + App.TOPBAR;
+        if (x != -1.0f && y != -1.0f) { // Only render if monster hasn't disappeared
+            int drawX = (int) (x * App.CELLSIZE + App.CELLSIZE / 2 - image.width / 2);
+            int drawY = (int) (y * App.CELLSIZE + App.CELLSIZE / 2 - image.height / 2 + App.TOPBAR);
             app.image(image, drawX, drawY);
         }
     }
-
 }
