661b24aecfdf0bf4506cf59fb67113db0c5ef7ae
Ian Chang <ycha5975@uni.sydney.edu.au>
2023-09-27 16:48:36 +1000

    fixed extended spawn delay

--- src/main/java/WizardTD/Wave.java
+++ src/main/java/WizardTD/Wave.java
@@ -37,8 +37,7 @@ public class Wave {
         
         // If it's time to spawn a new monster and there are still monsters left to spawn
         if (currentSpawnTime >= spawnInterval && monstersToSpawn > 0) {
-            int spawnDelay = Math.round(spawnInterval * monsters.size());
-            Monster newMonster = new Monster(board, app, speed, spawnDelay);
+            Monster newMonster = new Monster(board, app, speed, 0);
             monsters.add(newMonster);
             // Add the new monster to the activeMonsters list in App.java directly
             ((App) app).addActiveMonster(newMonster);
